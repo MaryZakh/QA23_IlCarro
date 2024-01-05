ccc841f12f660875a61535a9584465e681414779
Maria Z <z0559882272@gmail.com>
2024-01-05 15:00:31 +0200

    login success&model of User

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,10 +1,67 @@
 package tests;
 
+import models.User;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTests {
+public class LoginTests extends TestBase {
+
+    @BeforeMethod
+    public void preConditions() {
+        if (app.getHelperUser().isLogged()) {
+            app.getHelperUser().logout();
+        }
+
+    }
+
     @Test
-    public void test() {
+    public void loginSuccess1() {
+        User user = new User().withEmail("marga@gmail.com").withPassword("Mmar123456$");
+/*
+        user.setEmail("marga@gmail.com");
+        user.setPassword("Mmar123456$");
+*/
+
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submitLogin();
+        //Assert if element with text "Logged in success" is present
+        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+        //app.getHelperUser().clickOkButton();
+
+    }
+
+
+    @Test
+    public void loginSuccess() {
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("marga@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
+        //Assert if element with text "Logged in success" is present
+        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+        //app.getHelperUser().clickOkButton();
+
+
+    }
+
+    @Test
+    public void loginSuccessModel() {
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("marga@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
+        //Assert if element with text "Logged in success" is present
+        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+        //app.getHelperUser().clickOkButton();
+
+
+    }
+
+
+    @AfterMethod
+    public void postConditions() {
+        app.getHelperUser().clickOkButton();
 
     }
 }
